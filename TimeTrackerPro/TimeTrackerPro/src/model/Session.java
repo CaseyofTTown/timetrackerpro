b8c110f8831d2e668e02d8d20e2221de6e373f95
CaseyofTTown <cj.ras@hotmail.com>
2024-07-27 11:24:29 -0400

    added session class

--- TimeTrackerPro/TimeTrackerPro/src/model/Session.java
+++ TimeTrackerPro/TimeTrackerPro/src/model/Session.java
@@ -1,5 +1,41 @@
 package model;
 
+import java.time.Instant;
+
+//class for GUI to know status of user (signed in/out)
 public class Session {
+	private String loggedInUser;
+	private Instant lastActivity;
+	
+	public Session() {
+		
+	}
+	
+	public void login(String username) {
+		loggedInUser = username;
+		updateActivity();
+	}
+	
+	public void logout() {
+		loggedInUser = null;
+		lastActivity = null;
+		System.out.println("User logged out from Session");
+	}
+	
+	public String getLoggedInUser() {
+		return loggedInUser;
+	}
+	
+	public boolean isLoggedIn() {
+		return loggedInUser != null;
+	}
+	//call whenever a user performs an action to keep them signed in!
+	public void updateActivity() {
+		lastActivity = Instant.now();
+		System.out.println("lastActivity updated");
+	}
+	public Instant getLastActivity() {
+		return lastActivity;
+	}
 
 }
