32e706dcfcf9f7e20027ee4a95412a3c8d6c25bd
CaseyofTTown <cj.ras@hotmail.com>
2024-08-21 13:01:41 -0400

    updated database to be stored externally to maintain integrity during updates. database can now be found in users document folder under Time Tracker Pro folder that will be created by app if it doesnt exist.

--- TimeTrackerPro/TimeTrackerPro/src/TimeTrackerProApplication.java
+++ TimeTrackerPro/TimeTrackerPro/src/TimeTrackerProApplication.java
@@ -1,5 +1,9 @@
 import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import controller.TTController;
 import model.DatabaseManager;
@@ -7,36 +11,58 @@ import view.viewClass;
 
 public class TimeTrackerProApplication {
 
-	// db url
-	private static final String dbUrl = "jdbc:sqlite:timeTracker.db";
+    private static final String dbUrl = "jdbc:sqlite:" + System.getProperty("user.home") + "/Documents/TimeTrackerPro/timeTracker.db";
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("testing");
+        System.out.println("testing");
 
-		DatabaseManager db = createDatabaseManager();
-		if (db != null) {
-			System.out.println("Database manager Successfuly created!");
-		}
+        DatabaseManager db = createDatabaseManager();
+        if (db != null) {
+            System.out.println("Database manager Successfully created!");
+        }
 
-		// Create and display the GUI
-		SwingUtilities.invokeLater(() -> {
-			viewClass view = new viewClass();
-			view.setIconImage(new ImageIcon("src/timeTrackerLogo.ico").getImage()); // Set the icon here
-			view.setVisible(true);
+        // Create and display the GUI
+        SwingUtilities.invokeLater(() -> {
+            viewClass view = new viewClass();
+            view.setIconImage(new ImageIcon("src/timeTrackerLogo.ico").getImage()); // Set the icon here
+            view.setVisible(true);
 
-			// create the controller
-			TTController controller = new TTController(db, view);
-		});
+            // create the controller
+            TTController controller = new TTController(db, view);
+        });
 
-	}
+    }
 
-	private static DatabaseManager createDatabaseManager() {
-		try {
-			return new DatabaseManager(dbUrl);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			return null;
-		}
-	}
+    private static DatabaseManager createDatabaseManager() {
+        try {
+            File dbDir = new File(System.getProperty("user.home") + "/Documents/TimeTrackerPro");
+            if (!dbDir.exists()) {
+                dbDir.mkdirs();
+                System.out.println("Directory created: " + dbDir.getAbsolutePath());
+            } else {
+                System.out.println("Directory already exists: " + dbDir.getAbsolutePath());
+            }
+
+            File dbFile = new File(dbDir, "timeTracker.db");
+            if (!dbFile.exists()) {
+                try (InputStream is = TimeTrackerProApplication.class.getResourceAsStream("/timeTracker.db");
+                     FileOutputStream fos = new FileOutputStream(dbFile)) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = is.read(buffer)) != -1) {
+                        fos.write(buffer, 0, bytesRead);
+                    }
+                    System.out.println("Database file created: " + dbFile.getAbsolutePath());
+                }
+            } else {
+                System.out.println("Database file already exists: " + dbFile.getAbsolutePath());
+            }
+
+            return new DatabaseManager(dbUrl);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
